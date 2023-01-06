f77df6943844c5f39f27a9edfba5b9276b50c9b4
Sumeyra <smyryalcin@gmail.com>
2023-01-06 11:50:22 +0000

    updated testNG class

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -9,4 +9,10 @@ public class TestNG_Intro {
         System.out.println("Test 1 is running...");
 
     }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running...");
+
+    }
 }
