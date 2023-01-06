f4c583c087d442a0af2fb83313a389dbd1f7976c
Sumeyra <smyryalcin@gmail.com>
2023-01-06 11:02:21 +0000

    Rodio Button example Task

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefE.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T1_StaleElementRefE.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -25,9 +26,29 @@ public class T1_StaleElementRefE {
 
         Thread.sleep(2000);
         addElementButton.click();
+
         //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteButton = driver.findElement(By.xpath("//button[@class='added-manually']"));
+        System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+
+
+
+
+
         //5. Click to “Delete” button.
+        deleteButton.click();
+
+
         //6. Verify “Delete” button is NOT displayed after clicking.
         //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+
+       try {
+           System.out.println("deleteButton.isDisplayed() = " + deleteButton.isDisplayed());
+       }catch (StaleElementReferenceException e){
+           System.out.println("-->StaleElementReferenceException exception is thrown");
+           System.out.println("-->This means the web element is completely deleted from the page");
+           System.out.println("deleteButton.isDisplayed() = false");
+       }
+       driver.close();
     }
 }
