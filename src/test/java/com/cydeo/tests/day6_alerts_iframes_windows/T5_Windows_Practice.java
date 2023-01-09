86561d5f281dd816667ba29903df6863f88b610e
Sumeyra <smyryalcin@gmail.com>
2023-01-09 12:32:45 +0000

    Window Task

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_Windows_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to website: https://practice.cydeo.com/windows
+        driver.get("https://practice.cydeo.com/windows");
+    }
+
+    @Test
+    public void multiple_window_test(){
+
+        //Storing the main page's window handle as string is
+        // good practice for future re-usable purposes
+
+        //4. Assert: Title is “Windows”
+        //5. Click to: “Click Here” link
+        //6. Switch to new Window.
+        //7. Assert: Title is “New Window”
+
+    }
+}
