294b414189a97d74d42d4b9d9b9d9007f6f0129f
Sumeyra <smyryalcin@gmail.com>
2023-01-12 14:31:38 +0000

    order class created

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -0,0 +1,47 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WebTable_Order_Verify {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUPMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/web-tables");
+    }
+
+
+    @Test
+    public void order_name_verify_test() {
+
+        //Locate the cell that has Bob Martin text in it.
+
+        WebElement bobMartinCell=
+                driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[.='Bob Martin']"));
+
+        // we located as a web element
+        System.out.println("bobMartinCell "+ bobMartinCell.getText());
+
+
+        //2. Verify Bob’s name is listed as expected.
+        String expectedBobName = "Bob Martin";
+        String actualBobName = bobMartinCell.getText();
+
+        Assert.assertEquals(actualBobName, expectedBobName);
+        //Expected: “Bob Martin”
+        //3. Verify Bob Martin’s order date is as expected
+        //Expected: 12/31/2021
+    }
+}
