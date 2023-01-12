e8f1ae6b60e9321233a1e6d8407ed7623c58f3d3
Sumeyra <smyryalcin@gmail.com>
2023-01-12 12:26:29 +0000

    Browser Utils created

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,19 +1,76 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class T1_WindowHandling {
+
     //TC #1: Window Handle practice
+
+   public  WebDriver driver;
     // 1. Create new test and make set ups
-    //2. Go to : https://www.amazon.com
-    //3. Copy paste the lines from below into your class
-    //4. Create a logic to switch to the tab where Etsy.com is open
-    //5. Assert: Title contains “Etsy”
-    //
-    //Lines to be pasted:
-    //((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
-    //((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
-    //((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
-    //
-    //These lines will simply open new tabs using something called JavascriptExecutor
-    //and get those pages. We will learn JavascriptExecutor later as well.
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.SECONDS);
+
+    }
+
+   @Test
+    public void window_handling_test(){
+       //2. Go to : https://www.amazon.com
+       driver.get("https://www.amazon.com");
+
+       //3. Copy paste the lines from below into your class
+       ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+       ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+       ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+       
+       
+
+       Set<String> allWindowsHandles = driver.getWindowHandles();
+       for (String allWindowsHandle : allWindowsHandles) {
+
+           driver.switchTo().window(allWindowsHandle);
+
+           System.out.println("current url "+ driver.getCurrentUrl());
+
+           //4. Create a logic to switch to the tab where Etsy.com is open
+
+
+           if (driver.getCurrentUrl().contains("etsy")){
+               break;
+           }
+           
+       }
+       //5. Assert: Title contains “Etsy”
+
+       String actualTitle = driver.getTitle();
+       String expectedInTitle ="Etsy";
+
+       Assert.assertTrue(allWindowsHandles.contains("etsy"));
+       
+       //Lines to be pasted:
+      // ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+      // ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+       //((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+       //
+       //These lines will simply open new tabs using something called JavascriptExecutor
+       //and get those pages. We will learn JavascriptExecutor later as well.
+       
+       
+   }
+    
+
+
+    
     //
 }
