50f5d6693436536c31ed257e2ef723315aee70a6
Sumeyra <smyryalcin@gmail.com>
2023-01-18 14:42:20 +0000

    java faker 2

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -10,5 +10,18 @@ public class JavaFakerPractice {
         Faker faker = new Faker();
 
         System.out.println("faker.name().firstName() = " + faker.name().firstName());
+        System.out.println("faker.name().lastName() = " + faker.name().lastName());
+        System.out.println("faker.name().fullName() = " + faker.name().fullName());
+
+        //numerify() method will generate random numbers in the format we want to get
+        System.out.println("faker.numerify(\"###-###-####\") = " + faker.numerify("###-###-####"));
+
+        System.out.println("faker.numerify(\"312-###-####\") = "
+                + faker.numerify("312-###-####"));
+
+        //letterify() method will return random letters where we pass "?"
+        System.out.println("faker.letterify(\"???-????\") = " + faker.letterify("???-????"));
+
+
     }
 }
