18674855f8751c4cd31c90e8c58d5f4d00f68aea
Sumeyra <smyryalcin@gmail.com>
2023-01-19 14:05:48 +0000

    registration form

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -1,25 +1,73 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class T1_Registration_Form {
     @Test
-    public void registration_form_test(){}
-    //1. Open browser
-    //2. Go to website: https://practice.cydeo.com/registration_form
-    //3. Enter first name
-    //4. Enter last name
-    //5. Enter username
-    //6. Enter email address
-    //7. Enter password
-    //8. Enter phone number
-    //9. Select a gender from radio buttons
-    //10. Enter date of birth
-    //11. Select Department/Office
-    //12. Select Job Title
-    //13. Select programming language from checkboxes
-    //14. Click to sign up button
-    //15. Verify success message “You’ve successfully completed registration.” is
-    //displayed.
+    public void registration_form_test() {
+        //1. Open browser
+        //2. Go to website: https://practice.cydeo.com/registration_form
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("registration.form.url"));
+
+        //Create JavaFaker object
+        Faker faker = new Faker();
+
+        //3. Enter first name
+        WebElement inputFirstName =Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
+        inputFirstName.sendKeys(faker.name().firstName());
+
+        //4. Enter last name
+        WebElement inputLastName =Driver.getDriver().findElement(By.xpath("//input[@name='lastname']"));
+        inputLastName.sendKeys(faker.name().lastName());
+
+
+        //5. Enter username
+        //inputUsername.sendKeys(faker.name().username().replaceAll(".", ""));
+        WebElement inputUsername = Driver.getDriver().findElement(By.xpath("//input[@name='username']"));
+
+        String user = faker.bothify("helpdesk###");
+        inputUsername.sendKeys(user);
+
+        //6. Enter email address
+        WebElement inputEmail = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
+        inputEmail.sendKeys(user + "@email.com");
+
+
+        //7. Enter password
+        WebElement inputPassword = Driver.getDriver().findElement(By.xpath("//input[@name='password']"));
+        inputPassword.sendKeys(faker.numerify("########"));
+
+
+        //8. Enter phone number
+        WebElement inputPhone = Driver.getDriver().findElement(By.xpath("//input[@name='phone']"));
+        inputPhone.sendKeys(faker.numerify("###-###-####"));
+
+        //9. Select a gender from radio buttons
+        WebElement inputGender = Driver.getDriver().findElement(By.xpath("//input[@value='female']"));
+        inputGender.click();
+
+
+        //10. Enter date of birth
+        WebElement inputBirthday = Driver.getDriver().findElement(By.xpath("//input[@name='birthday']"));
+        //faker.number().numberBetween(2004, 1920)  bu sekilde numaraniu sinirlayabilirsin
+        inputBirthday.sendKeys("03/08/2000");
+
+
+        //11. Select Department/Office
+        Select departmentDropdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='department']")));
+        departmentDropdown.selectByIndex(faker.number().numberBetween(1,9));
+
+        //12. Select Job Title
+        //13. Select programming language from checkboxes
+        //14. Click to sign up button
+        //15. Verify success message “You’ve successfully completed registration.” is
+        //displayed.
+    }
 }
