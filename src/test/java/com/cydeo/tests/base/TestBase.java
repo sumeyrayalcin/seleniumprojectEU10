6ac4d4cc53a73c3213f9d0e421888e66155e52d2
Sumeyra <smyryalcin@gmail.com>
2023-01-19 10:03:16 +0000

    group study

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeMethod;
 import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
-    public WebDriver driver;
+    protected WebDriver driver;
 
     @BeforeMethod
     public void setupMethod(){
---
+++ src/test/java/com/cydeo/tests/group_study/Task1.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.group_study;
+
+public class Task1 {
+    //Write a basic browser automation framework to validate a Polymer website. The focus should
+    //be on The Web Application under test http://todomvc.com/
+    //1. The first step should be to load the website, click within the JavaScript tab, and select
+    //Polymer link.
+    //2. The second step should be: Add two Todo items
+    //3. Bonus: (optional stretch goal): Edit the content of the second Todo item
+
+
+
+}
+/*
+
+ */
