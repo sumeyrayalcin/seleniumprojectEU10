aefcfd8a42ba4904ebf47c278f07c11ac0e5514f
Sumeyra <smyryalcin@gmail.com>
2023-01-21 23:22:01 +0000

    SmartBears order completed

--- src/test/java/com/cydeo/tests/tasks_solutions/Day8_Task1SmartBear.java
+++ src/test/java/com/cydeo/tests/tasks_solutions/Day8_Task1SmartBear.java
@@ -6,6 +6,7 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -96,14 +97,57 @@ public class Day8_Task1SmartBear {
         inputState.sendKeys(faker.address().state());
 
         WebElement inputZipcode= driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox5']"));
-        inputZipcode.sendKeys(faker.address().zipCode());
+        inputZipcode.sendKeys(faker.numerify("#####"));
 
 
 
         //10. Click on “visa” radio button
+        WebElement visa = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_cardList_0']"));
+        visa.click();
+        Thread.sleep(1000);
+
         //11. Generate card number using JavaFaker
+        WebElement cardNumber = driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox6']"));
+        cardNumber.sendKeys(faker.numerify("################"));
+
+        WebElement expireDate =driver.findElement(By.xpath("//input[@id='ctl00_MainContent_fmwOrder_TextBox1']"));
+        expireDate.sendKeys(faker.number().numberBetween(10,12) +"/" + faker.number().numberBetween(10,30));
+
         //12. Click on “Process”
+        WebElement buttonProcess = driver.findElement(By.xpath("//a[@id='ctl00_MainContent_fmwOrder_InsertButton']"));
+        buttonProcess.click();
+
         //13. Verify success message “New order has been successfully added.”
+        String expectedMessage = "New order has been successfully added.";
+        WebElement actualMessage = driver.findElement(By.xpath("//strong"));
+        Assert.assertEquals(actualMessage.getText(),expectedMessage);
+
+    }
+
+    @Test
+    public void Susan(){
+        //TC#3: Smartbear software order verification
+        //1. Open browser and login to Smartbear software
+        //2. Click on View all orders
+        //3. Verify Susan McLaren has order on date “01/05/2010”
+        //1. Open browser
+        //2. Go to website:
+        //http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+
+        //3. Enter username: “Tester”
+        //4. Enter password: “test”
+        //5. Click to Login button
+        SmartBearUtils.loginToSmartBear(driver,"Tester","test");
+
+        //6. Print out count of all the links on landing page
+        //(Locate all the links in the page.)
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+
+        String actualDate = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr[6]//td[5]")).getText();
+        String expectedDate ="01/05/2010";
+
+        Assert.assertEquals(actualDate,expectedDate);
 
     }
 }
