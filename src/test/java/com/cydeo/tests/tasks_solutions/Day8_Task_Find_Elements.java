e384c7dc064972cf355e85d65f82bca3750abed7
Sumeyra <smyryalcin@gmail.com>
2023-01-21 23:31:38 +0000

    Find element class created

---
+++ src/test/java/com/cydeo/tests/tasks_solutions/Day8_Task_Find_Elements.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.tasks_solutions;
+
+import org.testng.annotations.Test;
+
+public class Day8_Task_Find_Elements {
+    @Test
+    public void FindElements(){
+
+        //TC #1: Checking the number of links on the page
+        //1. Open Chrome browser
+        //2. Go to https://www.openxcell.com
+        //3. Count the number of the links on the page and verify
+        //Expected: 332
+        //
+        //TC #2: Printing out the texts of the links on the page
+        //1. Open Chrome browser
+        //2. Go to https://www.openxcell.com
+        //3. Print out all of the texts of the links on the page
+        //
+        //TC #3: Counting the number of links that does not have text
+        //1. Open Chrome browser
+        //2. Go to https://www.openxcell.com
+        //3. Count the number of links that does not have text and verify
+        //Expected: 109
+        //
+        //NOTE: EXPECTED NUMBER OF LINKS FOR THIS PRACTICE MAY BE DIFFERENT
+        //FROM COMPUTER TO COMPUTER, OR FOR DIFFERENT OS (MAC-WINDOWS). SO
+        //IF YOU ARE GETTING A DIFFERENT NUMBER, JUST USE THAT NUMBER FOR
+        //VERIFICATION.
+    }
+}
