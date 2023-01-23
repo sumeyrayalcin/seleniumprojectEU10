13fd85c69493c6c82f08c761cb7510323e73bc60
Sumeyra <smyryalcin@gmail.com>
2023-01-23 13:30:22 +0000

    day 11 actions classes done

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -4,6 +4,7 @@ import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
@@ -24,5 +25,22 @@ public class T1_Circle_Drag_N_Drop {
         //2. Drag and drop the small circle to bigger circle.
         Actions actions = new Actions(Driver.getDriver());
 
+
+        //actions.dragAndDrop(smallCircle, bigCircle).perform();
+        actions.clickAndHold(smallCircle)
+                .pause(2000)
+                .moveToElement(bigCircle)
+                .pause(2000)
+                .release()
+                .perform();
+
+
+
+        //-Text in big circle changed to: “You did great!”
+
+        String actualBigCircleText = bigCircle.getText();
+        String expectedBigCircleText = "You did great!";
+
+        Assert.assertEquals(actualBigCircleText, expectedBigCircleText);
     }
     }
