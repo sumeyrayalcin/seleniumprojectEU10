6cd803bfd2c302232638cbd7e81a4f4c65964d1c
Sumeyra <smyryalcin@gmail.com>
2023-01-23 11:35:39 +0000

    action practice move to element

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -1,13 +1,34 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
 public class ActionsPractices {
 
-    //TC #4: Scroll practice
-    //1- Open a chrome browser
-    //2- Go to: https://practice.cydeo.com/
-    //3- Scroll down to “Powered by CYDEO”
-    //4- Scroll using Actions class “moveTo(element)” method
-    //
+    @Test
+    public void task_4_and_5_test(){
+        //TC #4: Scroll practice
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/
+        Driver.getDriver().get("https://practice.cydeo.com/");
+
+        //3- Scroll down to “Powered by CYDEO”
+        Actions actions = new Actions(Driver.getDriver());
+
+
+        //Locating cydeo link to be able to pass in the actions method
+        //a[@href='https://cydeo.com/']
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+
+
+        //4- Scroll using Actions class “moveTo(element)” method
+        actions.moveToElement(cydeoLink).perform();
+
+    }
+
     //
     //TC #5: Scroll practice 2
     //1- Continue from where the Task 4 is left in the same test.
