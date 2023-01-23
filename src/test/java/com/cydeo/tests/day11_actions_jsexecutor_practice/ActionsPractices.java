a683a672b6498718d8dbc17d25fc228de0290b43
Sumeyra <smyryalcin@gmail.com>
2023-01-23 11:26:10 +0000

    action practice start

---
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+public class ActionsPractices {
+
+    //TC #4: Scroll practice
+    //1- Open a chrome browser
+    //2- Go to: https://practice.cydeo.com/
+    //3- Scroll down to “Powered by CYDEO”
+    //4- Scroll using Actions class “moveTo(element)” method
+    //
+    //
+    //TC #5: Scroll practice 2
+    //1- Continue from where the Task 4 is left in the same test.
+    //2- Scroll back up to “Home” link using PageUP button
+}
