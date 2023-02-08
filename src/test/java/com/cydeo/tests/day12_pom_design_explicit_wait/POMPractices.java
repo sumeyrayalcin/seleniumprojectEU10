05b4061942c21d0348df7989dd0b7ebb66a07ced
Sumeyra <smyryalcin@gmail.com>
2023-02-08 12:29:18 +0000

    day 12 library pom practice

---
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.day12_pom_design_explicit_wait;
+
+import com.cydeo.pages.LibraryLoginPage;
+import com.cydeo.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class POMPractices {
+
+    @Test
+    public void required_field_error_message_test(){
+        Driver.getDriver().get("https://library1.cydeo.com/");
+        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+        libraryLoginPage.signInButton.click();
+        Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
+    }
+}
