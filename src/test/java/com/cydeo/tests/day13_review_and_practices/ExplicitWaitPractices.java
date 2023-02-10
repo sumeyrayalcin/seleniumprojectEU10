7f2feb4c8454df5fe7d1ffa72e1606298b2ed8b4
Sumeyra <smyryalcin@gmail.com>
2023-02-10 13:01:41 +0000

    day 13 ExplicitWaitPractices

---
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -0,0 +1,109 @@
+package com.cydeo.tests.day13_review_and_practices;
+
+import com.cydeo.pages.*;
+import com.cydeo.utilities.*;
+import org.openqa.selenium.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
+import java.util.concurrent.*;
+
+public class ExplicitWaitPractices {
+
+    DynamicControlsPage dynamicControlsPage;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
+        dynamicControlsPage = new DynamicControlsPage();
+
+    }
+
+    /*
+    TC #2: Explicit wait practice
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/dynamic_controls
+3- Click to “Remove” button
+4- Wait until “loading bar disappears”
+5- Verify:
+a. Checkbox is not displayed
+b. “It’s gone!” message is displayed.
+
+NOTE: FOLLOW POM
+     */
+    @Test
+    public void remove_button_test(){
+        //3- Click to “Remove” button
+        dynamicControlsPage.removeButton.click();
+
+        //4- Wait until “loading bar disappears”
+//        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+//        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+//        wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
+
+        //5- Verify:
+        //a. Checkbox is not displayed
+
+        try {
+            //assertFalse method will pass the test if the boolean value returned is: false
+            Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            Assert.assertTrue(!dynamicControlsPage.checkbox.isDisplayed());
+            Assert.assertFalse(dynamicControlsPage.checkbox.isDisplayed());
+        }catch (NoSuchElementException n){
+            Assert.assertTrue(true);
+        }
+
+        //b. “It’s gone!” message is displayed.
+        Assert.assertTrue(dynamicControlsPage.message.isDisplayed());
+        Assert.assertTrue(dynamicControlsPage.message.getText().equals("It's gone!"));
+    }
+
+    /*
+    //TC #3: Explicit wait practice
+//1- Open a chrome browser
+//2- Go to: https://practice.cydeo.com/dynamic_controls
+//3- Click to “Enable” button
+//4- Wait until “loading bar disappears”
+//5- Verify:
+//a. Input box is enabled.
+//b. “It’s enabled!” message is displayed.
+//
+//NOTE: FOLLOW POM
+     */
+    @Test
+    public void enable_button_test(){
+        //3- Click to “Enable” button
+
+        //System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+        //        + dynamicControlsPage.inputBox.isEnabled());
+
+        dynamicControlsPage.enableButton.click();
+
+        System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+                + dynamicControlsPage.inputBox.isEnabled());
+
+        //4- Wait until “loading bar disappears”
+        //Calling our ExplicitWait utility method to wait loadingBar to disappear
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
+
+        System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+                + dynamicControlsPage.inputBox.isEnabled());
+
+        //5- Verify:
+        //a. Input box is enabled.
+        Assert.assertTrue(dynamicControlsPage.inputBox.isEnabled());
+
+        //b. “It’s enabled!” message is displayed.
+        Assert.assertTrue(dynamicControlsPage.message.isDisplayed());
+
+        //Check the String value is matching as expected: “It’s enabled!”
+        Assert.assertTrue(dynamicControlsPage.message.getText().equals("It's enabled!"));
+
+    }
+
+}
+
+
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,6 +1,9 @@
 package com.cydeo.utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
@@ -64,5 +67,17 @@ public class BrowserUtils {
         wait.until(ExpectedConditions.invisibilityOf(webElement));
     }
 */
+
+
+
+    /*
+    Creating a utility method for ExplicitWait, so we don't have to repeat the lines
+     */
+    public static void waitForInvisibilityOf(WebElement webElement){
+        //Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);  optional
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
 
 }
